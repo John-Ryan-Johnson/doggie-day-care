1b9bbebe7254a4e8fe74eccca5f0f4dc7404be8a
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-30 19:06:57 -0600

    completed data for dogs and employees

---
+++ src/helpers/data/employeesData.js
@@ -0,0 +1,38 @@
+const employees = [
+  {
+    id: 'employee1',
+    firstName: 'Jaimee',
+    lastName: 'Crownover',
+    phoneNumber: '(555) 315-7207',
+  },
+  {
+    id: 'employee2',
+    firstName: 'Contessa',
+    lastName: 'Delaune',
+    phoneNumber: '(555) 657-5900',
+  },
+  {
+    id: 'employee3',
+    firstName: 'Bert',
+    lastName: 'Hessling',
+    phoneNumber: '(555) 980-8567',
+  },
+  {
+    id: 'employee4',
+    firstName: 'Howard',
+    lastName: 'Vassar',
+    phoneNumber: '(555) 782-1162',
+  },
+  {
+    id: 'employee5',
+    firstName: 'Izetta',
+    lastName: 'Corr',
+    phoneNumber: '(555) 241-9747',
+  },
+  {
+    id: 'employee6',
+    firstName: 'Norman',
+    lastName: 'Gershon',
+    phoneNumber: '(555) 575-4354',
+  },
+];
