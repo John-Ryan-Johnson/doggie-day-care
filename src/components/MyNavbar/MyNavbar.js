15ca0d15f4c5ab6581c4f5befc671e1c77d7c3fc
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-01-03 13:09:04 -0600

    added walk and styling

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import './MyNavbar.scss';
-
 
 class MyNavbar extends React.Component {
   static propTypes = {
@@ -17,16 +17,24 @@ class MyNavbar extends React.Component {
 
   render() {
     const { authed } = this.props;
-
     return (
-      <div className="Navbar">
-        <nav className="navbar navbar-expand-sm navbar-custom">
-          <h1 className="navbar-brand text-white">Ryan's Kennel</h1>
-          <div className="form-inline my-2 my-lg-0">
-
-            {
-              authed && (<button className="btn btn-danger" onClick={ this.logMeOut }>Logout</button>)
-            }
+      <div className="MyNavbar">
+        <nav className="navbar navbar-expand-lg navbar-custom">
+          <span className="navbar-brand" href="#">Ryan's Doggie Daycare</span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDem" aria-controls="navbarTogglerDem" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDem">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            </ul>
+            <div className="form-inline my-2 my-lg-0">
+              {/* If authed, show logout button */}
+              {/* If NOT authed, show nothing */}
+              {
+                // can just have && if only trying to show 1 thing in terniary
+            authed && (<button className="nav-link" onClick={ this.logMeOut }>Logout</button>)
+              }
+            </div>
           </div>
         </nav>
       </div>
